ea2b03bf999722f2f2681c8fb48ed422df8c2af2
RaghvendraJha007 <raghvendrajham@gmail.com>
2023-04-24 20:20:15 +0530

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Assumenda
+        dolores facere doloribus at asperiores eum non perspiciatis nulla
+        obcaecati libero? Consectetur hic numquam rerum eveniet adipisci. Sit
+        vitae ad ut. Praesentium nihil doloremque explicabo voluptate aliquam
+        consequuntur culpa nostrum sit eligendi quis! Esse fugit, laboriosam
+      </p>
+    </section>
+  );
+};
+
+export default About;
